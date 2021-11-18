211533795292663f209f541cd4f10ea1162ca840
Juan Ávila <juanantonio.avila@masmovil.com>
2021-11-18 20:44:10 +0100

    Initial commit

---
+++ src/main/kotlin/es/urjc/realfood/restaurants/RealfoodRestaurantsApplication.kt
@@ -0,0 +1,11 @@
+package es.urjc.realfood.restaurants
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealfoodRestaurantsApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealfoodRestaurantsApplication>(*args)
+}
