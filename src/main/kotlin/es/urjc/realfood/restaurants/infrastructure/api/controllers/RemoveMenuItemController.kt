2e7b60af0806e6db95feab203e3197808bf9b7e8
Juan Avila <dev@juanavila.es>
2022-06-17 15:07:05 +0200

    [Restaurants] Deploy v1.2.1

--- src/main/kotlin/es/urjc/realfood/restaurants/infrastructure/api/controllers/RemoveMenuItemController.kt
+++ src/main/kotlin/es/urjc/realfood/restaurants/infrastructure/api/controllers/RemoveMenuItemController.kt
@@ -1,20 +1,18 @@
 package es.urjc.realfood.restaurants.infrastructure.api.controllers
 
-import es.urjc.realfood.restaurants.application.addmenuitem.AddMenuItemRequest
 import es.urjc.realfood.restaurants.application.removemenuitem.RemoveMenuItem
 import es.urjc.realfood.restaurants.application.removemenuitem.RemoveMenuItemRequest
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class RemoveMenuItemController(private val removeMenuItem: RemoveMenuItem) {
 
-    @PostMapping("/api/restaurants/{restaurantId}/menu/items/{itemId}")
+    @DeleteMapping("/api/restaurants/{restaurantId}/menu/items/{itemId}")
     fun postRemoveMenuItem(
         @PathVariable restaurantId: String,
-        @RequestBody menuItemRequest: AddMenuItemRequest, @PathVariable itemId: String,
+        @PathVariable itemId: String,
     ) = removeMenuItem(
         RemoveMenuItemRequest(
             restaurantId = restaurantId,
